4013fa7725030883fd5e942049662b16c23f73af
Wojciech Rzóska <wojciechrzoska@gmail.com>
2022-12-31 11:25:41 +0100

    forward ref

--- src/components/molecules/FormField/FormField.js
+++ src/components/molecules/FormField/FormField.js
@@ -3,32 +3,28 @@ import PropTypes from 'prop-types';
 import styles from './FormField.module.scss';
 import Input from 'components/atoms/Input/Input';
 
-const FormField = ({
-  value,
-  onChange,
-  label,
-  name,
-  id,
-  type = 'text',
-  ...props
-}) => {
-  return (
-    <div className={styles.wrapper}>
-      <label className={styles.label} htmlFor={id}>
-        {label}
-      </label>
-      <Input
-        className={styles.input}
-        name={name}
-        id={id}
-        type={type}
-        value={value}
-        onChange={onChange}
-        data-testid={label}
-      />
-    </div>
-  );
-};
+const FormField = React.forwardRef(
+  ({ onChange, value, label, name, id, type = 'text', ...props }, ref) => {
+    return (
+      <div className={styles.wrapper}>
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+        <Input
+          className={styles.input}
+          name={name}
+          id={id}
+          type={type}
+          value={value}
+          onChange={onChange}
+          data-testid={label}
+          {...props}
+          ref={ref}
+        />
+      </div>
+    );
+  }
+);
 
 FormField.propTypes = {
   label: PropTypes.string.isRequired,
