d9806cd9131026346ee45203f6497433ba5ef6cc
Wojciech Rzóska <wojciechrzoska@gmail.com>
2023-01-16 15:39:43 +0100

    refactor to other files

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useParams, Link, Navigate } from 'react-router-dom';
 import styledWrapper from 'assets/styles/ViewWrapper.module.scss';
 import styles from './Dashboard.module.scss';
@@ -8,37 +8,32 @@ import { Title } from 'components/atoms/Title/Title';
 import useModal from 'components/organisms/Modal/useModal';
 import StudentDetails from 'components/molecules/StudentDetails/StudentDetails';
 import Modal from 'components/organisms/Modal/Modal';
+import { useGetGroupsQuery } from 'store';
 
 const Dashboard = () => {
-  const [groups, setGroups] = useState([]);
   const [currentStudent, setCurrentStudent] = useState(null);
-  const { getGroups, getStudentById } = useStudents();
+  const { getStudentById } = useStudents();
   const { id } = useParams();
   const { isOpen, handleOpenModal, handleCloseModal } = useModal();
-
-  useEffect(() => {
-    (async () => {
-      const groups = await getGroups();
-      console.log(groups);
-      setGroups(groups);
-    })();
-  }, [getGroups]);
+  const { data, isLoading } = useGetGroupsQuery();
 
   const handleOpenStudentDetails = async (id) => {
     const student = await getStudentById(id);
     setCurrentStudent(student);
     handleOpenModal();
   };
-
-  if (!id && groups.length > 0)
-    return <Navigate to={`/group/${groups[0].id}`} />;
+  if (isLoading) {
+    return <div>Loadingg...</div>;
+  }
+  if (!id && data.groups.length > 0)
+    return <Navigate to={`/group/${data.groups[0].id}`} />;
 
   return (
     <div className={styles.Wrapper}>
       <div className={styles.titleWrapper}>
         <Title>Group {id}</Title>
         <nav>
-          {groups.map(({ id }) => (
+          {data.groups.map(({ id }) => (
             <Link key={id} to={`/group/${id}`}>
               {id}{' '}
             </Link>
